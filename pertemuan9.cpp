9ef68875fa1c76388811c7dcf4e346b3a884d0d6
FathurRahman0 <rahmanfthr165@gmail.com>
2025-05-15 19:22:38 +0700

    step pertama dalam penghapusan

--- pertemuan9.cpp
+++ pertemuan9.cpp
@@ -93,6 +93,17 @@ public:
 
         Node *current = START;
 
+        // step 1: Traverse the list to find the node
+            while (current != NULL && current->noMhs != rollNo)
+                current = current->next;
+
+            if (current == NULL)
+            {
+                cout << "Record not found" << endl;
+                return;
+            }
+
+
     }
 
 
