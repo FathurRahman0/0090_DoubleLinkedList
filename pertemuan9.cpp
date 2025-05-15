edc58149a907b2a12bef0c802b5f521606c81f9d
FathurRahman0 <rahmanfthr165@gmail.com>
2025-05-15 18:46:21 +0700

    membuat class DoubleLinkedlist

--- pertemuan9.cpp
+++ pertemuan9.cpp
@@ -9,3 +9,26 @@ public:
     Node *next;
     Node *prev;
 };
+
+class DoubleLinkedList
+{
+private:
+    Node *START;
+
+public:
+    DoubleLinkedList()
+    {
+        START = NULL;
+    }
+
+    void addNode()
+    {
+        int nin;
+        cout << "\nEnter the roll number of the student: ";
+        cin >> nim;
+
+        // Step 1 : ALLcote memory for new node
+        Node
+    } *newNode ->noMhs = nim;
+
+}
